f3b5fbda78ad190dd3bc2196020c89741caec33f
Nobodylesszb <nobodylesszb@163.com>
2019-04-13 00:31:44 +0800

    新增单例，工厂，原型，建设设计模式

---
+++ creations/factory/Beverage.py
@@ -0,0 +1,22 @@
+class Beverage():
+    name = ""
+    price = 0.0
+    type = "BEVERAGE"
+    def getPrice(self):
+        return self.price
+    def setPrice(self, price):
+        self.price = price
+    def getName(self):
+        return self.name
+
+
+class coke(Beverage):
+    def __init__(self):
+        self.name = "coke"
+        self.price = 4.0
+
+
+class milk(Beverage):
+    def __init__(self):
+        self.name = "milk"
+        self.price = 5.0
